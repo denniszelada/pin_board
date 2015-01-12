9c012f2fe482dd94d1394d2bde6c63037ac639b2
Dennis <denniszelada@gmail.com>
2015-01-11 22:02:42 -0600

    first commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,25 @@
+class PinsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new Pin"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+end
